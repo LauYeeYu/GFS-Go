bb86d6f60ee5f22448e67a995a8d295ef4404573
Lau Yee-Yu <liuyiyu2003@126.com>
2023-08-27 20:31:30 +0800

    feat(lib/client): calculate the stuff about offset

--- lib/client/file.go
+++ lib/client/file.go
@@ -33,3 +33,22 @@ func (client *Client) getFileChunks(file *File) ([]gfs.ChunkHandle, error) {
 		return nil, errors.New("invalid request")
 	}
 }
+
+// offsetToIndex converts the offset to the index of the chunk and the offset
+// in the chunk. Return the index and the offset in the chunk.
+func offsetToIndex(offset gfs.Length) (int, gfs.Length) {
+	return int(offset / gfs.ChunkSize), offset % gfs.ChunkSize
+}
+
+// getChunkHandleAt returns the chunk handle, the offset in the chunk and
+// the error (if exists) at the given offset.
+func (client *Client) getChunkHandleAt(
+	file *File, offset gfs.Length,
+) (gfs.ChunkHandle, gfs.Length, error) {
+	chunks, err := client.getFileChunks(file)
+	if err != nil {
+		return -1, -1, err
+	}
+	index, offsetInChunk := offsetToIndex(offset)
+	return chunks[index], offsetInChunk, nil
+}
